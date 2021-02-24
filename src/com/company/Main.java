78e2a77e3274ca704071648ebee3ec0f0b68d81a
golad730 <GoladGH2020>
2021-02-24 13:01:32 -0500

    Snap crackle pop commit from intelij IDE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Write a program that prints out the numbers 1 to 100 (inclusive).
         If the number is divisible by 3, print Crackle instead of the number.
         If it's divisible by 5, print Pop. If it's divisible by both 3 and 5,
-        print CracklePop. You can use any language. this is better version.
+        print CracklePop. You can use any language.
          */
 
         for (int i =1; i<=100; i++){
@@ -18,20 +18,27 @@ public class Main {
             j = i%3;
             k = i%5;
 
+            // Condition for Crackle pop
             if ((j==0)&&(k==0)){
                 System.out.println("CracklePop");
 
+                // Condition for Pop
             }else {
                 if ((i!=0)&&(k==0)){
                     System.out.println("Pop");
 
                 }else {
 
+                    // Condition for Crackle
                     if ((i != 0) && (j == 0)) {
                         System.out.println("Crackle");
-                    } else {
+                    }
 
-                        System.out.println("i: " + i + ", j: " + j + ", k: " + k);
+                    // Print statement
+                    else {
+
+                        //System.out.println("i: " + i + ", j: " + j + ", k: " + k);
+                        System.out.println( i );
                     }
                 }
             }
